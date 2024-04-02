d3179dd2a1b38e3ca565a14683be1374de791120
Taeyoung Won <hexter1994@gmail.com>
2024-04-02 20:07:40 +0900

    [Feat] 아이템 리스트 조작 기능 구현, transform 바인딩 구현

--- SeSACRxThreads/Shopping/ShoppingViewModel.swift
+++ SeSACRxThreads/Shopping/ShoppingViewModel.swift
@@ -13,12 +13,24 @@ final class ShoppingViewModel: ViewModel {
   
   // MARK: - I / O
   struct Input {
-    
+    let addItem: PublishRelay<ShopItem>
+    let deleteItem: PublishRelay<IndexPath>
+    let checkboxTapEvent: PublishRelay<Int>
+    let bookmarkTapEvent: PublishRelay<Int>
   }
   
   struct Output {
-    
+    let items: Driver<[ShopItem]>
   }
+  
+  private var items: [ShopItem] = [] {
+    didSet {
+      itemsRelay.accept(items)
+    }
+  }
+  
+  // MARK: - Observable
+  private lazy var itemsRelay = BehaviorRelay<[ShopItem]>(value: items)
   
   // MARK: - Property
   let disposeBag = DisposeBag()
@@ -26,6 +38,46 @@ final class ShoppingViewModel: ViewModel {
   // MARK: - Method
   func transform(input: Input) -> Output {
     
-    return Output()
+    input.addItem
+      .subscribe(with: self) { owner, item in
+        owner.addItem(with: item)
+      }
+      .disposed(by: disposeBag)
+    
+    input.deleteItem
+      .subscribe(with: self) { owner, indexPath in
+        owner.deleteItem(at: indexPath)
+      }
+      .disposed(by: disposeBag)
+    
+    input.checkboxTapEvent
+      .subscribe(with: self) { owner, index in
+        owner.toggleCheckbox(at: index)
+      }
+      .disposed(by: disposeBag)
+    
+    input.bookmarkTapEvent
+      .subscribe(with: self) { owner, index in
+        owner.toggleBookmark(at: index)
+      }
+      .disposed(by: disposeBag)
+    
+    return Output(items: itemsRelay.asDriver())
+  }
+  
+  private func addItem(with item: ShopItem) {
+    items.insert(item, at: 0)
+  }
+  
+  private func deleteItem(at indexPath: IndexPath) {
+    items.remove(at: indexPath.row)
+  }
+  
+  private func toggleCheckbox(at index: Int) {
+    items[index].isDone.toggle()
+  }
+  
+  private func toggleBookmark(at index: Int) {
+    items[index].bookmark.toggle()
   }
 }
