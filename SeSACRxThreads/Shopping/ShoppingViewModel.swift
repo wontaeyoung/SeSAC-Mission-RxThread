4b6b1c47f74f795e55614666507309a497c562d3
Taeyoung Won <hexter1994@gmail.com>
2024-04-02 20:05:26 +0900

    [Add] ShoppingViewModel 클래스 추가

---
+++ SeSACRxThreads/Shopping/ShoppingViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  ShoppingViewModel.swift
+//  SeSACRxThreads
+//
+//  Created by 원태영 on 4/2/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class ShoppingViewModel: ViewModel {
+  
+  // MARK: - I / O
+  struct Input {
+    
+  }
+  
+  struct Output {
+    
+  }
+  
+  // MARK: - Property
+  let disposeBag = DisposeBag()
+  
+  // MARK: - Method
+  func transform(input: Input) -> Output {
+    
+    return Output()
+  }
+}
