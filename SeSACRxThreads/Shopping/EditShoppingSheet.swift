2f4fbdab671f5ce6e0198844c20ad5d8a6f04983
Taeyoung Won <hexter1994@gmail.com>
2024-04-02 21:22:30 +0900

    [Feat] 아이템 수정 뷰컨트롤러 구현

---
+++ SeSACRxThreads/Shopping/EditShoppingSheet.swift
@@ -0,0 +1,81 @@
+//
+//  EditShoppingSheet.swift
+//  SeSACRxThreads
+//
+//  Created by 원태영 on 4/2/24.
+//
+
+import UIKit
+import KazUtility
+import RxSwift
+import RxCocoa
+
+final class EditShoppingSheetController: RxBaseViewController {
+  
+  // MARK: - UI
+  private let inputField = SignTextField(placeholderText: "무엇을 구매하실건가요?")
+  private let editButton = UIButton().configured {
+    $0.configuration = .filled().configured {
+      $0.title = "수정하기"
+      $0.cornerStyle = .medium
+      $0.buttonSize = .medium
+    }
+  }
+  
+  // MARK: - Property
+  let itemRelay: BehaviorRelay<ShopItem>
+  
+  // MARK: - Initializer
+  init(item: ShopItem) {
+    self.itemRelay = .init(value: item)
+    
+    super.init()
+  }
+  
+  // MARK: - Life Cycle
+  override func setHierarchy() {
+    view.addSubviews(inputField, editButton)
+  }
+  
+  override func setConstraint() {
+    inputField.snp.makeConstraints { make in
+      make.centerY.equalTo(view)
+      make.leading.equalTo(view).inset(20)
+      make.height.equalTo(50)
+    }
+    
+    editButton.snp.makeConstraints { make in
+      make.top.equalTo(inputField)
+      make.leading.equalTo(inputField.snp.trailing).offset(10)
+      make.trailing.equalTo(view).inset(20)
+      make.height.equalTo(50)
+    }
+  }
+  
+  override func bind() {
+    itemRelay
+      .map { $0.name }
+      .bind(to: inputField.rx.text)
+      .disposed(by: disposeBag)
+    
+    itemRelay
+      .bind(with: self) { owner, _ in
+        owner.dismiss(animated: true)
+      }
+      .disposed(by: disposeBag)
+    
+    editButton.rx.tap
+      .withLatestFrom(inputField.rx.text.orEmpty)
+      .bind(with: self, onNext: { owner, newName in
+        owner.updateItem(text: newName)
+      })
+      .disposed(by: disposeBag)
+  }
+  
+  // MARK: - Method
+  private func updateItem(text: String) {
+    var mutableItem = itemRelay.value
+    mutableItem.name = text
+    itemRelay.accept(mutableItem)
+  }
+}
