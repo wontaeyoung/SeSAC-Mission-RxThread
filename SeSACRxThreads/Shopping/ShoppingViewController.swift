d5650f82a9a066d67c31ad77af8610ea4c6b812a
Taeyoung Won <hexter1994@gmail.com>
2024-04-02 20:12:36 +0900

    [Feat] UI 레이아웃 구현

--- SeSACRxThreads/Shopping/ShoppingViewController.swift
+++ SeSACRxThreads/Shopping/ShoppingViewController.swift
@@ -13,7 +13,21 @@ import RxCocoa
 final class ShoppingViewController: RxBaseViewController, ViewModelController {
   
   // MARK: - UI
+  private let inputField = SignTextField(placeholderText: "무엇을 구매하실건가요?")
+  private let addButton = UIButton().configured {
+    $0.configuration = .filled().configured {
+      $0.title = "추가하기"
+      $0.cornerStyle = .medium
+      $0.buttonSize = .medium
+    }
+  }
   
+  private lazy var tableView = UITableView().configured {
+    $0.register(ShoppingTableCell.self, forCellReuseIdentifier: ShoppingTableCell.identifier)
+    $0.rowHeight = 60
+    $0.separatorStyle = .none
+    $0.keyboardDismissMode = .onDrag
+  }
   
   // MARK: - Property
   let viewModel: ShoppingViewModel
@@ -27,21 +41,30 @@ final class ShoppingViewController: RxBaseViewController, ViewModelController {
   
   // MARK: - Life Cycle
   override func setHierarchy() {
-    
+    view.addSubviews(inputField, addButton, tableView)
   }
   
   override func setConstraint() {
+    inputField.snp.makeConstraints { make in
+      make.top.equalTo(view.safeAreaLayoutGuide)
+      make.leading.equalTo(view).inset(20)
+      make.height.equalTo(50)
+    }
     
-  }
-  
-  override func setAttribute() {
+    addButton.snp.makeConstraints { make in
+      make.top.equalTo(inputField)
+      make.leading.equalTo(inputField.snp.trailing).offset(10)
+      make.trailing.equalTo(view).inset(20)
+      make.height.equalTo(50)
+    }
     
+    tableView.snp.makeConstraints { make in
+      make.top.equalTo(inputField.snp.bottom).offset(20)
+      make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+    }
   }
   
   override func bind() {
     
   }
-  
-  // MARK: - Method
-  
 }
