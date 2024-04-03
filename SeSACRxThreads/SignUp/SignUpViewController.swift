ba672380b4a9ea97a405d4d42add50a1d254296d
Taeyoung Won <hexter1994@gmail.com>
2024-04-03 18:14:58 +0900

    [Feat] 이메일 유효성 검사 바인딩 적용

--- SeSACRxThreads/SignUp/SignUpViewController.swift
+++ SeSACRxThreads/SignUp/SignUpViewController.swift
@@ -7,62 +7,88 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
-class SignUpViewController: UIViewController {
-
-    let emailTextField = SignTextField(placeholderText: "이메일을 입력해주세요")
-    let validationButton = UIButton()
-    let nextButton = PointButton(title: "다음")
+final class SignUpViewController: UIViewController {
+  
+  private let emailTextField = SignTextField(placeholderText: "이메일을 입력해주세요").configured {
+    $0.spellCheckingType = .no
+    $0.autocorrectionType = .no
+    $0.autocapitalizationType = .none
+    $0.keyboardType = .emailAddress
+  }
+  
+  private let nextButton = PointButton(title: "다음")
+  private let validationInfoLabel = UILabel()
+  
+  private let viewModel = SignUpViewModel()
+  private let disposeBag = DisposeBag()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    view.backgroundColor = Color.white
+    
+    configureLayout()
+    bind()
+  }
 
-        view.backgroundColor = Color.white
-        
-        configureLayout()
-        configure()
-        
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
-
+  private func bind() {
+    let input = SignUpViewModel.Input(email: .init())
+    let output = viewModel.transform(input: input)
+    
+    output.validation
+      .drive(nextButton.rx.isEnabled)
+      .disposed(by: disposeBag)
+    
+    output.validation
+      .map { $0 ? "사용 가능한 이메일이에요" : "이메일 형식에 맞지 않아요" }
+      .drive(validationInfoLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+    output.validation
+      .map { $0 ? UIColor.systemGreen : UIColor.systemRed }
+      .drive(validationInfoLabel.rx.textColor)
+      .disposed(by: disposeBag)
+    
+    nextButton.rx.tap
+      .bind(with: self) { owner, _ in
+        owner.navigationController?.pushViewController(PasswordViewController(), animated: true)
+      }
+      .disposed(by: disposeBag)
+    
+    emailTextField.rx.text.orEmpty
+      .bind(to: input.email)
+      .disposed(by: disposeBag)
+    
+    emailTextField.rx.text.orEmpty
+      .map { $0.isEmpty }
+      .bind(to: validationInfoLabel.rx.isHidden)
+      .disposed(by: disposeBag)
+  }
+  
+  func configureLayout() {
+    view.addSubview(emailTextField)
+    view.addSubview(nextButton)
+    view.addSubview(validationInfoLabel)
+    
+    emailTextField.snp.makeConstraints { make in
+      make.height.equalTo(50)
+      make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+      make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
     }
     
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(PasswordViewController(), animated: true)
-    }
-
-    func configure() {
-        validationButton.setTitle("중복확인", for: .normal)
-        validationButton.setTitleColor(Color.black, for: .normal)
-        validationButton.layer.borderWidth = 1
-        validationButton.layer.borderColor = Color.black.cgColor
-        validationButton.layer.cornerRadius = 10
+    validationInfoLabel.snp.makeConstraints { make in
+      make.top.equalTo(emailTextField.snp.bottom).offset(5)
+      make.leading.equalTo(emailTextField)
+      make.height.equalTo(20)
     }
     
-    func configureLayout() {
-        view.addSubview(emailTextField)
-        view.addSubview(validationButton)
-        view.addSubview(nextButton)
-        
-        validationButton.snp.makeConstraints { make in
-            make.height.equalTo(50)
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
-            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
-            make.width.equalTo(100)
-        }
-        
-        emailTextField.snp.makeConstraints { make in
-            make.height.equalTo(50)
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
-            make.leading.equalTo(view.safeAreaLayoutGuide).inset(20)
-            make.trailing.equalTo(validationButton.snp.leading).offset(-8)
-        }
-        
-        nextButton.snp.makeConstraints { make in
-            make.height.equalTo(50)
-            make.top.equalTo(emailTextField.snp.bottom).offset(30)
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
-        }
+    nextButton.snp.makeConstraints { make in
+      make.height.equalTo(50)
+      make.top.equalTo(validationInfoLabel.snp.bottom).offset(30)
+      make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
     }
-    
-
+  }
 }
