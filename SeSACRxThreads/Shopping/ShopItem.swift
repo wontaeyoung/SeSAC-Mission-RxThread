7453ffb65af6e8aa5ba01284a22fc9bde9b1ddda
Taeyoung Won <hexter1994@gmail.com>
2024-04-03 16:27:14 +0900

    [Fix] UUID 프로퍼티 추가

--- SeSACRxThreads/Shopping/ShopItem.swift
+++ SeSACRxThreads/Shopping/ShopItem.swift
@@ -5,13 +5,17 @@
 //  Created by 원태영 on 4/2/24.
 //
 
-struct ShopItem: Equatable {
+import Foundation
+
+struct ShopItem {
   
+  let id: UUID
   var name: String
   var isDone: Bool
   var bookmark: Bool
   
   init(name: String) {
+    self.id = UUID()
     self.name = name
     self.isDone = false
     self.bookmark = false
