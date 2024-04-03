eb945a5380b34699c174d864ad9cf90c72c59543
Taeyoung Won <hexter1994@gmail.com>
2024-04-03 21:27:45 +0900

    [Feat] 패스워드 유효성 검사 바인딩 구현

--- SeSACRxThreads/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -12,12 +12,20 @@ import RxCocoa
 
 class PasswordViewController: UIViewController {
   
-  let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
-  let nextButton = PointButton(title: "다음")
+  private let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요").configured {
+    $0.isSecureTextEntry = true
+  }
   
-  let descriptionLabel = UILabel()
-  let validText = Observable.just("8자 이상 입력해주세요")
-  let disposebag = DisposeBag()
+  private let confirmPasswordTextField = SignTextField(placeholderText: "비밀번호를 한번 더 입력해주세요").configured {
+    $0.isSecureTextEntry = true
+  }
+  
+  private let passwordValidationLabel = UILabel()
+  private let confirmPasswordValidationLabel = UILabel()
+  private let nextButton = PointButton(title: "다음")
+  
+  private let disposebag = DisposeBag()
+  private let viewModel = PasswordViewModel()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -28,14 +36,14 @@ class PasswordViewController: UIViewController {
     bind()
   }
   
-  private func showNextView() {
-    navigationController?.pushViewController(PhoneViewController(), animated: true)
-  }
-  
   func configureLayout() {
-    view.addSubview(passwordTextField)
-    view.addSubview(descriptionLabel)
-    view.addSubview(nextButton)
+    view.addSubviews(
+      passwordTextField,
+      confirmPasswordTextField,
+      passwordValidationLabel,
+      confirmPasswordValidationLabel,
+      nextButton
+    )
     
     passwordTextField.snp.makeConstraints { make in
       make.height.equalTo(50)
@@ -43,36 +51,76 @@ class PasswordViewController: UIViewController {
       make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
     }
     
-    descriptionLabel.snp.makeConstraints { make in
-      make.top.equalTo(passwordTextField.snp.bottom).offset(10)
-      make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+    passwordValidationLabel.snp.makeConstraints { make in
       make.height.equalTo(20)
+      make.top.equalTo(passwordTextField.snp.bottom).offset(5)
+      make.leading.equalTo(passwordTextField)
+    }
+    
+    confirmPasswordTextField.snp.makeConstraints { make in
+      make.height.equalTo(50)
+      make.top.equalTo(passwordValidationLabel.snp.bottom).offset(20)
+      make.horizontalEdges.equalTo(passwordTextField)
+    }
+    
+    confirmPasswordValidationLabel.snp.makeConstraints { make in
+      make.height.equalTo(20)
+      make.top.equalTo(confirmPasswordTextField.snp.bottom).offset(5)
+      make.leading.equalTo(passwordTextField)
     }
     
     nextButton.snp.makeConstraints { make in
       make.height.equalTo(50)
-      make.top.equalTo(descriptionLabel.snp.bottom).offset(20)
+      make.top.equalTo(confirmPasswordValidationLabel.snp.bottom).offset(20)
       make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
     }
   }
   
   func bind() {
-    validText
-      .bind(to: descriptionLabel.rx.text)
+    let input = PasswordViewModel.Input(password: .init(), confirmPassword: .init())
+    let output = viewModel.transform(input: input)
+    
+    output.lengthValidation
+      .map { $0 ? "사용 가능한 비밀번호에요" : "비밀번호는 8자 이상 16자 이하만 가능해요" }
+      .drive(passwordValidationLabel.rx.text)
       .disposed(by: disposebag)
     
-    let validation = passwordTextField.rx.text.orEmpty
-      .map { $0.count }
-      .map { $0 >= 8 }
-      .share(replay: 1)
-    
-    validation
-      .map { $0 ? UIColor.systemPink : UIColor.lightGray }
-      .bind(to: nextButton.rx.backgroundColor)
+    output.lengthValidation
+      .map { self.validationColor(condition: $0) }
+      .drive(passwordValidationLabel.rx.textColor)
       .disposed(by: disposebag)
     
-    validation
-      .bind(to: nextButton.rx.isEnabled, descriptionLabel.rx.isHidden)
+    output.equalValidation
+      .map { $0 ? "" : "비밀번호가 일치하지 않아요" }
+      .drive(confirmPasswordValidationLabel.rx.text)
+      .disposed(by: disposebag)
+    
+    output.equalValidation
+      .map { self.validationColor(condition: $0) }
+      .drive(confirmPasswordValidationLabel.rx.textColor)
+      .disposed(by: disposebag)
+    
+    output.validation
+      .debug()
+      .drive(nextButton.rx.isEnabled)
+      .disposed(by: disposebag)
+    
+    passwordTextField.rx.text.orEmpty
+      .bind(to: input.password)
+      .disposed(by: disposebag)
+    
+    passwordTextField.rx.text.orEmpty
+      .map { $0.isEmpty }
+      .bind(to: passwordValidationLabel.rx.isHidden)
+      .disposed(by: disposebag)
+    
+    confirmPasswordTextField.rx.text.orEmpty
+      .bind(to: input.confirmPassword)
+      .disposed(by: disposebag)
+    
+    confirmPasswordTextField.rx.text.orEmpty
+      .map { $0.isEmpty }
+      .bind(to: confirmPasswordValidationLabel.rx.isHidden)
       .disposed(by: disposebag)
     
     nextButton.rx.tap
@@ -80,5 +128,13 @@ class PasswordViewController: UIViewController {
         owner.showNextView()
       }
       .disposed(by: disposebag)
+  }
+  
+  private func validationColor(condition: Bool) -> UIColor {
+    return condition ? .systemGreen : .systemRed
+  }
+  
+  private func showNextView() {
+    navigationController?.pushViewController(ShoppingViewController(), animated: true)
   }
 }
